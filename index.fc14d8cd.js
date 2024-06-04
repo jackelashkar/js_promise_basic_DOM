04409945c79ce581b3e3b988fda0de3b46e4508d
Jack Ashkar <jackelashkar@gmail.com>
2024-06-04 11:03:12 +0300

    make build

---
+++ index.fc14d8cd.js
@@ -0,0 +1,2 @@
+var e=new Promise(function(e,t){document.querySelector(".logo").addEventListener("click",function(){e()})}),t=new Promise(function(e,t){setTimeout(function(){t()},3e3)});function n(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.appendChild(e)}function o(){var e=document.createElement("div");e.className="message error-message",e.textContent="Promise was rejected!",document.body.appendChild(e)}e.then(n).catch(o),t.then(n).catch(o);
+//# sourceMappingURL=index.fc14d8cd.js.map
